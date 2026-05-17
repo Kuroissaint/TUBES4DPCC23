0b21f8b84b746656d4f75d0565d828f5cafee728
Firda Ridzki Utami <ridzkiutamifirda@gmail.com>
2026-05-17 15:38:24 +0700

    Add files via upload

---
+++ account-service/test/auth_functional_test.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	"account-service/handler"
+	"account-service/model"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type MockAuthService struct{}
+
+func (m MockAuthService) Login(req model.LoginRequest) (*model.LoginResponse, error) {
+	return &model.LoginResponse{
+		Token: "dummy-token",
+	}, nil
+}
+
+func TestLogin_Functional(t *testing.T) {
+
+	mockService := MockAuthService{}
+	authHandler := handler.NewAuthHandler(mockService)
+
+	body := []byte(`{
+		"email":"user@mail.com",
+		"password":"123"
+	}`)
+
+	req := httptest.NewRequest(
+		"POST",
+		"/api/auth/login",
+		bytes.NewBuffer(body),
+	)
+
+	req.Header.Set("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+
+	authHandler.LoginHandler(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected 200, got %d", rr.Code)
+	}
+
+	var response model.LoginResponse
+
+	err := json.Unmarshal(rr.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Failed parse response: %v", err)
+	}
+
+	if response.Token != "dummy-token" {
+		t.Errorf("Expected dummy-token, got %s", response.Token)
+	}
+}
