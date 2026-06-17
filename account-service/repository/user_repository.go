3a461caee465d1e88b75c4a43a697ad7e0afd433
Firda Ridzki Utami <ridzkiutamifirda@gmail.com>
2026-06-17 17:04:39 +0700

    Update user_repository.go

--- account-service/repository/user_repository.go
+++ account-service/repository/user_repository.go
@@ -4,4 +4,7 @@ import "account-service/model"
 
 type UserRepository interface {
 	GetByEmail(email string) (*model.User, error)
+	Register(user model.User) (*model.User, error)
+	RegisterDriver(user model.User, driver model.DriverProfile) (*model.User, error)
+	RegisterCustomer(user model.User, alamat string) (*model.User, error)
 }
